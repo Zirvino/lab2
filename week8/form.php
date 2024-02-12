08da53b21aa0af3e3ebdb86ff5c7f203c4652bef
Yang <92528481+Zirvino@users.noreply.github.com>
2024-02-13 02:00:31 +0800

    Update form.php with socitcloud

--- week8/form.php
+++ week8/form.php
@@ -121,19 +121,19 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM p-mariano_myguests";
-$result = $conn->query($sql);
+$sql = "INSERT INTO jlquesada_myguests (name, email,website,comment,gender)
+VALUES ('$name', '$email','$website','$comment','$gender')";
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ". $row["email"]. " " . $row["website"]. $row["comment"]. $row["gender"]. "      " . "<br>";
-  }
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-  echo "0 results";
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 $conn->close();
+}
 ?>
+
 
 
 </body>
