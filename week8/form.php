3624c1d8e6048a9538e21e294f61d3768950ec9e
Yang <92528481+Zirvino@users.noreply.github.com>
2024-02-13 02:20:34 +0800

    Update form.php updated socit cloud connection

--- week8/form.php
+++ week8/form.php
@@ -114,25 +114,25 @@ $password = "xE*Y2nleNVvZm[!!";
 $dbname = "webprogmi222_sf221";
 ?>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-$sql = "INSERT INTO jlquesada_myguests (name, email,website,comment,gender)
-VALUES ('$name', '$email','$website','$comment','$gender')";
+  $sql = "INSERT INTO p_mariano_myguests (name, email, website, comment, gender) VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
-$conn->close();
+  $conn->close();
 }
 ?>
+  
 
 
 
