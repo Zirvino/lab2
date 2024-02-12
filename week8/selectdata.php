0be7d9c04851720051a09c7d78a716dba99e6b02
Yang <92528481+Zirvino@users.noreply.github.com>
2024-02-13 02:45:36 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM p-mariano_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM `p-mariano_myguests`";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
